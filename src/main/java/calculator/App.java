f0e7c53aa1ce36da17796f9ea9933ba1b51e95ea
Robbie <robbie331@>
2024-04-26 19:34:49 +0900

    2-2 App 클래스에 Calculator클래스 사칙연산 활용 & DivideException 예외처리

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -1,19 +1,18 @@
 package calculator;
 
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.Scanner;
+import java.sql.SQLOutput;
+import java.util.*;
 
 public class App {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws DivideException {
 //        /*연산 결과 10개를 저장할 길이 int배열 객체 생성*/
 //        int[] calculationResult = new int[10];
 //        /*연산 결과를 index에 저장하면서 카운트 초기값*/
 //        int idx = 0;
+//
+//        /* 10개 이상 결과 무한 저장 Queue 생성*/
+//        Queue<Integer> calculationResult = new LinkedList<>();
 
-        /* 10개 이상 결과 무한 저장 Queue 생성*/
-        Queue<Integer> calculationResult = new LinkedList<>();
 
         Scanner sc = new Scanner(System.in);
         /* exit 문자열이 입력될 때 까지 연산 반복 반복문*/
@@ -30,27 +29,38 @@ public class App {
             int num2 = sc.nextInt();
 
 
-            int result = 0; // 연산 결과 초기값
-            /*연산 시작*/
-            if (operator == '/' && num2 == 0) { // 분모가 0인 나눗셈은 연산 없이 경고문구 출력
-                System.out.println("나눗셈 연산에서 분모에 0이 입력될 수 없습니다.");
-            } else {
-                /* 입력된 operator 값 + - * / 에 따라 각각 연산 후, 결과값 출력*/
-                switch (operator) {
-                    case '+':
-                        result = num1 + num2;
-                        break;
-                    case '-':
-                        result = num1 - num2;
-                        break;
-                    case '*':
-                        result = num1 * num2;
-                        break;
-                    case '/':
-                        result = num1 / num2;
-                        break;
-                }
-                System.out.println("결과: " + result);
+//            int result = 0; // 연산 결과 초기값
+//            /*연산 시작*/
+//            if (operator == '/' && num2 == 0) { // 분모가 0인 나눗셈은 연산 없이 경고문구 출력
+//                System.out.println("나눗셈 연산에서 분모에 0이 입력될 수 없습니다.");
+//            } else {
+//                /* 입력된 operator 값 + - * / 에 따라 각각 연산 후, 결과값 출력*/
+//                switch (operator) {
+//                    case '+':
+//                        result = num1 + num2;
+//                        break;
+//                    case '-':
+//                        result = num1 - num2;
+//                        break;
+//                    case '*':
+//                        result = num1 * num2;
+//                        break;
+//                    case '/':
+//                        result = num1 / num2;
+//                        break;
+//                }
+//                System.out.println("결과: " + result);
+
+            /*Calculator의 DivideException Handling*/
+            try {
+                /*Calculator 클래스를 활용한 사칙연산*/
+                Deque<Double> calculationResult = Calculator.calculate(num1, num2, operator);
+                /*최근 연산값 출력*/
+                System.out.println("결과: " + calculationResult.peekLast());
+            } catch (DivideException e1) {
+                System.out.println(e1.getMessage());
+            }
+
 
 //                /*인덱스 9초과를 했을 경우, 처음 값을 지우고 앞으로 당겨 마지막에 저장*/
 //                if (idx > 9) {
@@ -64,35 +74,34 @@ public class App {
 //                    calculationResult[idx] = result;
 //                    idx++; // 다음 반복시 다음 인덱스에 넣기 위에 증가 연산자
 //                }
+//
+//                /*calculationResult 연산저장*/
+//                calculationResult.add(result);
 
-                /*calculationResult 연산저장*/
-                calculationResult.add(result);
-
-                /*remove입력시 calculationResult 최초 연산 삭제*/
-                System.out.println("가장 먼저 저장된 연산 결과를 삭제하시겠습니까? (remove 입력 시 삭제)");
-                input = sc.next();
-                if (input.equals("remove")) {
-                    calculationResult.poll(); // 가장 처음값 꺼내서 없애기
-                }
-
-
-                /*inquiry입력시 calculationResult 전부 출력*/
-                System.out.println("저장된 연산결과를 조회하시겠습니까? (inquiry 입력 시 조회)");
-                input = sc.next();
-                if (input.equals("inquiry")) {
-                    for(int resultIndex : calculationResult) {
-                        System.out.println(resultIndex); // 한 줄씩 출력
-                    }
-                }
-
-
-                /*추가 연산 여부 확인*/
-                System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
-                input = sc.next();
+            /*remove입력시 calculationResult 최초 연산 삭제*/
+            System.out.println("가장 먼저 저장된 연산 결과를 삭제하시겠습니까? (remove 입력 시 삭제)");
+            input = sc.next();
+            if (input.equals("remove")) {
+                Calculator.calculationResult.pollFirst(); // 가장 처음값 꺼내서 없애기
             }
+
+
+            /*inquiry입력시 calculationResult 전부 출력*/
+            System.out.println("저장된 연산결과를 조회하시겠습니까? (inquiry 입력 시 조회)");
+            input = sc.next();
+            if (input.equals("inquiry")) {
+                for (double resultIndex : Calculator.calculationResult) {
+                    System.out.println(resultIndex); // 한 줄씩 출력
+                }
+            }
+
+
+            /*추가 연산 여부 확인*/
+            System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
+            input = sc.next();
+
         } while (!(input.equals("exit"))); // exit이 입력되면 반복문 종료, 그 외에는 계속 반복와
 
-        System.out.println(calculationResult); // 배열 저장 확인
 
     }
 }
